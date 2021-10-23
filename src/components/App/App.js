b2cc91a7d04080a86267412667b58e96f7b524da
Vania Tavares <vntavares56@gmail.com>
2021-10-24 00:48:23 +0100

    small debugging app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
-import logo from '../../logo.svg';
+
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
-import Business from '../Business/Business';
+
 
 
 function App() {
